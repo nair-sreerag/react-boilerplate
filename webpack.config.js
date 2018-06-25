1cb9078f62399ef2f6d5b1d8f1deadef532e6ce2
Sreerag <nair_sreerag@outlook.com>
2018-06-25 18:14:08 +0530

    Css is being handled perfectly

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode : "development",
@@ -18,7 +18,11 @@ module.exports = {
     module: {
         rules: [
           { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-          { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+          { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+          { test : /\.css$/, use : ["style-loader", "css-loader"] }
         ]
-      }
-}
+    },
+    // plugins: [
+    //     new HtmlWebpackPlugin(),
+    // ],
+}   
