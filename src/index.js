e22e2429db498c269fba971e7f06d4eb3a339641
Sreerag <nair_sreerag@outlook.com>
2018-06-25 18:29:19 +0530

    Added webpack watcher to package.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Test from './Test'
 
 ReactDOM.render(
-     React.createElement("h1",null,"Hello world"),                  
-    //<Test />,                //<-- name of thr test app
+    //  React.createElement("h1",null,"Hello world"),                  
+    <Test />,                //<-- name of thr test app
     document.getElementById("root")
 );
