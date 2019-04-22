f252bdc47abc9a3e4574f52d57519ffc738141ad
Sreerag Nair <sreerag.nair@built.io>
2019-04-22 21:22:59 +0530

    added support for component generation for linux systems

--- automate/_generateFile.js
+++ automate/_generateFile.js
@@ -2,11 +2,47 @@ const fs = require("fs");
 const path = require('path')
 
 
-// globals
-global.pathToRoot = process.cwd()
-global.pathToSrc = pathToRoot + "\\src"
+
+let os = process.platform
+
+let writeFilePath = undefined
+let openSyncPath = undefined
+
 
 const args = process.argv.slice(2)
+
+switch (os) {
+
+    //Mac
+    case 'darwin':
+        // ??
+        break;
+
+    //windows - even for 64bit
+    case 'win32':
+        global.pathToRoot = process.cwd()
+        global.pathToSrc = pathToRoot + "\\src"
+        writeFilePath = pathToSrc + `\\${args[0]}Component.js`
+        openSyncPath = pathToSrc + `\\${args[0]}Component.css`
+        break;
+
+    case 'linux':
+        global.pathToRoot = process.cwd()
+        global.pathToSrc = pathToRoot + "/src"
+        writeFilePath = pathToSrc + `/${args[0]}Component.js`
+        openSyncPath = pathToSrc + `/${args[0]}Component.css`
+        break;
+
+    case 'freebsd':
+        break;
+
+    case 'sunos':
+        break;
+
+    default:
+        throw new Error("Some unidentified OS found!")
+
+}
 
 const template = `
 import React , { Component } from 'react';
@@ -15,6 +51,7 @@ import './${args[0]}Component.css'
 const ${args[0]}Component = () =>{
     return (
         //start writing here...
+        <span>${args[0]}Component has been generated!</span>
     )
 }
 
@@ -26,20 +63,20 @@ function generateComponent() {
     //check if the folder exists...
     if (fs.existsSync(pathToSrc)) {
         //save it in the directory
-        fs.writeFile(pathToSrc + `\\${args[0]}Component.js`, template, function (err) {
+        fs.writeFile(writeFilePath, template, function (err) {
             if (err) console.error(err)
             //create a css file with the equivalent name
-            fs.closeSync(fs.openSync(pathToSrc + `\\${args[0]}Component.css`, 'w'))
+            fs.closeSync(fs.openSync(openSyncPath, 'w'))
         })
     } else {
         //create src and save it in the directory
         fs.mkdir(pathToSrc, function (err) {
             if (err) console.error(err)
 
-            fs.writeFile(pathToSrc + `\\${args[0]}Component.js`, template, function (err) {
+            fs.writeFile(writeFilePath, template, function (err) {
                 if (err) console.error(err)
                 //create a css file with the equivalent name
-                fs.closeSync(fs.openSync(pathToSrc + `\\${args[0]}Component.css`, 'w'))
+                fs.closeSync(fs.openSync(openSyncPath, 'w'))
             })
         })
     }
