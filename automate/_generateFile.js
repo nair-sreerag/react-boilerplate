a8f7a5dec66e5d8cb98beded17a48f2588489296
nair-sreerag <nair_sreerag@outlook.com>
2019-04-23 20:28:40 +0530

    handled os variations and component generation + its error handling

--- automate/_generateFile.js
+++ automate/_generateFile.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require('path')
-
+const chalk = require("chalk")
 
 
 let os = process.platform
@@ -9,80 +9,112 @@ let writeFilePath = undefined
 let openSyncPath = undefined
 
 
-const args = process.argv.slice(2)
+try {
 
-switch (os) {
+    var args = process.argv.slice(2)
+    let isComponentCall = false;
 
-    //Mac
-    case 'darwin':
-        // ??
-        break;
+    if (args[0] == "component") isComponentCall = true
 
-    //windows - even for 64bit
-    case 'win32':
-        global.pathToRoot = process.cwd()
-        global.pathToSrc = pathToRoot + "\\src"
-        writeFilePath = pathToSrc + `\\${args[0]}Component.js`
-        openSyncPath = pathToSrc + `\\${args[0]}Component.css`
-        break;
+    // component type is specified but not the name
+    if (isComponentCall && !args[1]) throw new Error("Please specify a component name.")
 
-    case 'linux':
-        global.pathToRoot = process.cwd()
-        global.pathToSrc = pathToRoot + "/src"
-        writeFilePath = pathToSrc + `/${args[0]}Component.js`
-        openSyncPath = pathToSrc + `/${args[0]}Component.css`
-        break;
+    // if everything is valid for component generation
+    else if (isComponentCall && args[1]) {
+        console.log("Component call!!!!!")
+    }
+    // just to generate a simple file
+    else if (!isComponentCall && args[1]) throw new Error("Please use the 'component' keyword to generate a component template or just enter the filename to generate a React file.")
 
-    case 'freebsd':
-        break;
 
-    case 'sunos':
-        break;
+    switch (os) {
 
-    default:
-        throw new Error("Some unidentified OS found!")
+        //Mac
+        case 'darwin':
+            // ??
+            break;
 
-}
+        //windows - even for 64bit
+        case 'win32':
+            global.pathToRoot = process.cwd()
+            global.pathToSaveComponent = pathToRoot + (isComponentCall ? "\\src\\components" : "\\src")
+            // global.pathToComponent = pathToRoot + `\\component\\${args[1]}`
+            writeFilePath = pathToSaveComponent + `\\${isComponentCall ? args[1] : args[0]}Component.js`
+            openSyncPath = pathToSaveComponent + `\\${isComponentCall ? args[1] : args[0]}Component.css`
+            break;
 
-const template = `
-import React , { Component } from 'react';
-import './${args[0]}Component.css'
+        case 'linux':
+            global.pathToRoot = process.cwd()
+            global.pathToSaveComponent = pathToRoot + (isComponentCall ? "/src/components" : "/src")
+            // global.pathToComponent = pathToRoot + `/component/${args[1]}`
+            writeFilePath = pathToSaveComponent + `/${isComponentCall ? args[1] : args[0]}Component.js`
+            openSyncPath = pathToSaveComponent + `/${isComponentCall ? args[1] : args[0]}Component.css`
+            break;
 
-const ${args[0]}Component = () =>{
-    return (
-        //start writing here...
-        <span>${args[0]}Component has been generated!</span>
-    )
-}
+        case 'freebsd':
+            break;
 
-export default ${args[0]}Component;
-`
+        case 'sunos':
+            break;
 
-function generateComponent() {
+        default:
+            throw new Error("Some unidentified OS found!")
 
-    //check if the folder exists...
-    if (fs.existsSync(pathToSrc)) {
-        //save it in the directory
-        fs.writeFile(writeFilePath, template, function (err) {
-            if (err) console.error(err)
-            //create a css file with the equivalent name
-            fs.closeSync(fs.openSync(openSyncPath, 'w'))
-        })
-    } else {
-        //create src and save it in the directory
-        fs.mkdir(pathToSrc, function (err) {
-            if (err) console.error(err)
+    }
 
+
+    const template = `
+        import React , { Component } from 'react';
+        import './${args[0]}Component.css'
+        
+        const ${args[0]}Component = () =>{
+            return (
+                //start writing here...
+                <span>${args[0]}Component has been generated!</span>
+            )
+        }
+        
+        export default ${args[0]}Component;
+    `
+
+
+
+    function generateComponent() {
+
+        console.log(`global.pathToRoot : ${global.pathToRoot},
+                     global.pathToSaveComponent : ${global.pathToSaveComponent},
+                     writeFilePath : ${writeFilePath},
+                     openSyncPath : ${openSyncPath}`)
+
+
+        //check if the folder exists...
+        if (fs.existsSync(pathToSaveComponent)) {
+            //save it in the directory
             fs.writeFile(writeFilePath, template, function (err) {
                 if (err) console.error(err)
                 //create a css file with the equivalent name
                 fs.closeSync(fs.openSync(openSyncPath, 'w'))
             })
-        })
+        } else {
+            console.log("Doesnt exist")
+            //create src or component and save it in the directory
+            fs.mkdir(pathToSaveComponent, function (err) {
+                if (err) console.error(err)
+
+                fs.writeFile(writeFilePath, template, function (err) {
+                    if (err) console.error(err)
+                    //create a css file with the equivalent name
+                    fs.closeSync(fs.openSync(openSyncPath, 'w'))
+                })
+            })
+        }
+
+
     }
 
+    generateComponent()
 
 }
-
-
-generateComponent()
+catch (err) {
+    console.error(chalk.red(err.message))
+}
